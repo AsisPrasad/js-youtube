a527f83199ef7d1a4b748d7ef2f22f1b94a30b0f
AsisPrasad <84381060+AsisPrasad@users.noreply.github.com>
2025-03-25 10:24:08 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+console.log("2" > 1);
+console.log(undefined>=0);
+
+// === *strict check
+
+console.log("8" === 1); //strict check will strictly check the comparison, 
+// datatypes wiil have to be considered//
