2dd0b75e4e7d1bdab8337425628e25eed601b5ef
AsisPrasad <84381060+AsisPrasad@users.noreply.github.com>
2025-03-25 11:07:39 +0000

    DataTypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const number = 55
+console.log(number);
+console.log(typeof number);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
