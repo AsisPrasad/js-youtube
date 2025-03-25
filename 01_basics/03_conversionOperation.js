d3b5ff53f441164600b9d69d319d922aaf78137a
AsisPrasad <84381060+AsisPrasad@users.noreply.github.com>
2025-03-25 06:14:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = 33
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan (Not a number)
+// true =>1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) //converting no. into boolean
+console.log(booleanIsLoggedIn);
+
+// For Boolean: 
+// 1 =>true; 0 =>false
+// "" =>false
+// "Prasad" => true
+
+let anyNumber = 55
+
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//node 01_basics/03_conversionOperation.js//
+
+
+
