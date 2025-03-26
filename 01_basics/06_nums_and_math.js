2e304e8eebc0084bc78cbc7c1e4f9819e7a9f1e2
AsisPrasad <84381060+AsisPrasad@users.noreply.github.com>
2025-03-26 11:37:58 +0000

    maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -5,22 +5,23 @@ const balance = new Number(100)
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1));          //It will give fixed value after decimal
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));  //It will give precised value, mostly considering 
+                                           // value before decimal                   
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));  //Will give values having commas like 10,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.2));  // It will give greater value than the whole no. like it gives "5"
+// console.log(Math.floor(4.9)); //It will give lower value than the whole no. like it gives "4"
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
